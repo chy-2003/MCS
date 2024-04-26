9a5898614ebf09fde2dd3675ea5c5d5928b9148e
chy-2003 <chy_2003@foxmail.com>
2024-04-26 14:18:44 +0800

    Update

--- MonteCarlo.cuh
+++ MonteCarlo.cuh
@@ -9,9 +9,24 @@ __global__ void GetEnergy(double *ans, rMesh *mesh, SuperCell *superCell, double
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     int z = blockIdx.z;
+    int X, Y, Z;
     int n = threadIdx.z;
-    int id = x * (superCell->b * superCell->c) + y * (superCell->c) + z;
-    ReductionTemp[id] = Cal933(superCell->unitCell->Dots[n].A, (mesh->Unit)[id].Dots[n], (mesh->Unit)[id].Dots[n]);
+    int id1 = x * (superCell->b * superCell->c) + y * (superCell->c) + z;
+    int Id = x * (superCell->b * superCell->c * superCell->unitCell->N) + 
+              y * (superCell->c * superCell->unitCell->N) + 
+              z * (superCell->unitCell->N) + 
+              n;
+    int id2;
+    ReductionTemp[Id] = Cal933(superCell->unitCell->Dots[n].A, (mesh->Unit)[id1].Dots[n], (mesh->Unit)[id1].Dots[n]);
+    Bond *bond = superCell->unitCell->Dots->bonds;
+    while (bond != NULL) {
+        X = x + bond->Gx; if (X < 0) X += superCell->a; if (X >= superCell->a) X -= superCell->a;
+        Y = y + bond->Gy; if (Y < 0) Y += superCell->b; if (Y >= superCell->b) Y -= superCell->b;
+        Z = z + bond->Gz; if (Z < 0) Z += superCell->c; if (Z >= superCell->c) Z -= superCell->c;
+        id2 = X * (superCell->b * superCell->c) + Y * (superCell->c) + Z;
+        ReductionTemp[Id] += Cal933(bond->A, (mesh->Unit)[id1].Dots[n], (mesh->Unit)[id2].Dots[bond->t]);
+        bond = bond->Next;
+    }
     return;
 }
 
